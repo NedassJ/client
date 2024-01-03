330d2117e0210fa9ac9d441e36dda30a81e9b927
FlipReck <100430692+FlipReck@users.noreply.github.com>
2024-01-03 17:58:19 +0200

    BackEnd

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, Typography, Box } from '@mui/material';
+import { Button, Typography, Box, Alert } from '@mui/material';
 import GameGrid from '../components/grid';
 
 const App = () => {
   const [gameState, setGameState] = useState(Array.from({ length: 10 }, () => Array(10).fill(null)));
-
-
   const [gameId, setGameId] = useState(null);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [movesLeft, setMovesLeft] = useState(25);
 
   const startGame = async () => {
     try {
       const { data } = await axios.post('http://localhost:5000/start');
       setGameState(data.grid);
       setGameId(data.gameId);
+      setGameStarted(true); // Set the game as started
+      setMovesLeft(25); // Reset moves when a new game starts
+      console.log("Game started with ID:", data.gameId);
     } catch (error) {
       console.error('Error starting game:', error);
     }
@@ -27,22 +30,28 @@ const App = () => {
         y,
       });
       setGameState(data.grid);
-      // Check for game over or other messages here.
+      setMovesLeft(data.movesLeft); 
+      console.log(data.message)
     } catch (error) {
       console.error('Error making guess:', error);
     }
   };
 
   return (
-    <div>
-      <Box sx={{bgcolor:'#1b262c', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', height:'1000px'}}>
+    <Box sx={{bgcolor:'#1b262c', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', height:'1000px'}}>
       <Typography variant='h1' sx={{paddingTop: 2, paddingBottom: 2, color: '#e1f3ff'}}>Laivų mūšis</Typography>
       <GameGrid gameState={gameState} makeGuess={makeGuess} />
-      <Button variant="contained" onClick={startGame} sx={{marginTop: 2, bgcolor: '#0f4c75', '&:hover': { background: '#0f4c75', opacity: 0.75 }}}>
-        Pradėti žaidimą
-      </Button>
-      </Box>
-    </div>
+      {!gameStarted && (
+        <Button variant="contained" onClick={startGame} sx={{marginTop: 2, bgcolor: '#0f4c75', '&:hover': { background: '#0f4c75', opacity: 0.75 }}}>
+          Pradėti žaidimą
+        </Button>
+      )}
+      {gameStarted && (
+        <Alert severity="success" sx={{marginTop: 2}}>
+          Žaidimas prasidėjo! Liko ėjimų: {movesLeft}
+        </Alert>
+      )}
+    </Box>
   );
 };
 
